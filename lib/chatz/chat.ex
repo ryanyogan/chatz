947045d7bb4dec0a5f192973b99567065293d8af
ryan <ryanyogan@gmail.com>
2021-07-13 20:14:30 -0500

    added conversation members

--- lib/chatz/chat.ex
+++ lib/chatz/chat.ex
@@ -101,4 +101,100 @@ defmodule Chatz.Chat do
   def change_conversation(%Conversation{} = conversation, attrs \\ %{}) do
     Conversation.changeset(conversation, attrs)
   end
+
+  alias Chatz.Chat.ConversationMember
+
+  @doc """
+  Returns the list of chat_conversation_members.
+
+  ## Examples
+
+      iex> list_chat_conversation_members()
+      [%ConversationMember{}, ...]
+
+  """
+  def list_chat_conversation_members do
+    Repo.all(ConversationMember)
+  end
+
+  @doc """
+  Gets a single conversation_member.
+
+  Raises `Ecto.NoResultsError` if the Conversation member does not exist.
+
+  ## Examples
+
+      iex> get_conversation_member!(123)
+      %ConversationMember{}
+
+      iex> get_conversation_member!(456)
+      ** (Ecto.NoResultsError)
+
+  """
+  def get_conversation_member!(id), do: Repo.get!(ConversationMember, id)
+
+  @doc """
+  Creates a conversation_member.
+
+  ## Examples
+
+      iex> create_conversation_member(%{field: value})
+      {:ok, %ConversationMember{}}
+
+      iex> create_conversation_member(%{field: bad_value})
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def create_conversation_member(attrs \\ %{}) do
+    %ConversationMember{}
+    |> ConversationMember.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  @doc """
+  Updates a conversation_member.
+
+  ## Examples
+
+      iex> update_conversation_member(conversation_member, %{field: new_value})
+      {:ok, %ConversationMember{}}
+
+      iex> update_conversation_member(conversation_member, %{field: bad_value})
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def update_conversation_member(%ConversationMember{} = conversation_member, attrs) do
+    conversation_member
+    |> ConversationMember.changeset(attrs)
+    |> Repo.update()
+  end
+
+  @doc """
+  Deletes a conversation_member.
+
+  ## Examples
+
+      iex> delete_conversation_member(conversation_member)
+      {:ok, %ConversationMember{}}
+
+      iex> delete_conversation_member(conversation_member)
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def delete_conversation_member(%ConversationMember{} = conversation_member) do
+    Repo.delete(conversation_member)
+  end
+
+  @doc """
+  Returns an `%Ecto.Changeset{}` for tracking conversation_member changes.
+
+  ## Examples
+
+      iex> change_conversation_member(conversation_member)
+      %Ecto.Changeset{data: %ConversationMember{}}
+
+  """
+  def change_conversation_member(%ConversationMember{} = conversation_member, attrs \\ %{}) do
+    ConversationMember.changeset(conversation_member, attrs)
+  end
 end
--- test/chatz/chat_test.exs
+++ test/chatz/chat_test.exs
@@ -61,4 +61,63 @@ defmodule Chatz.ChatTest do
       assert %Ecto.Changeset{} = Chat.change_conversation(conversation)
     end
   end
+
+  describe "chat_conversation_members" do
+    alias Chatz.Chat.ConversationMember
+
+    @valid_attrs %{owner: true}
+    @update_attrs %{owner: false}
+    @invalid_attrs %{owner: nil}
+
+    def conversation_member_fixture(attrs \\ %{}) do
+      {:ok, conversation_member} =
+        attrs
+        |> Enum.into(@valid_attrs)
+        |> Chat.create_conversation_member()
+
+      conversation_member
+    end
+
+    test "list_chat_conversation_members/0 returns all chat_conversation_members" do
+      conversation_member = conversation_member_fixture()
+      assert Chat.list_chat_conversation_members() == [conversation_member]
+    end
+
+    test "get_conversation_member!/1 returns the conversation_member with given id" do
+      conversation_member = conversation_member_fixture()
+      assert Chat.get_conversation_member!(conversation_member.id) == conversation_member
+    end
+
+    test "create_conversation_member/1 with valid data creates a conversation_member" do
+      assert {:ok, %ConversationMember{} = conversation_member} = Chat.create_conversation_member(@valid_attrs)
+      assert conversation_member.owner == true
+    end
+
+    test "create_conversation_member/1 with invalid data returns error changeset" do
+      assert {:error, %Ecto.Changeset{}} = Chat.create_conversation_member(@invalid_attrs)
+    end
+
+    test "update_conversation_member/2 with valid data updates the conversation_member" do
+      conversation_member = conversation_member_fixture()
+      assert {:ok, %ConversationMember{} = conversation_member} = Chat.update_conversation_member(conversation_member, @update_attrs)
+      assert conversation_member.owner == false
+    end
+
+    test "update_conversation_member/2 with invalid data returns error changeset" do
+      conversation_member = conversation_member_fixture()
+      assert {:error, %Ecto.Changeset{}} = Chat.update_conversation_member(conversation_member, @invalid_attrs)
+      assert conversation_member == Chat.get_conversation_member!(conversation_member.id)
+    end
+
+    test "delete_conversation_member/1 deletes the conversation_member" do
+      conversation_member = conversation_member_fixture()
+      assert {:ok, %ConversationMember{}} = Chat.delete_conversation_member(conversation_member)
+      assert_raise Ecto.NoResultsError, fn -> Chat.get_conversation_member!(conversation_member.id) end
+    end
+
+    test "change_conversation_member/1 returns a conversation_member changeset" do
+      conversation_member = conversation_member_fixture()
+      assert %Ecto.Changeset{} = Chat.change_conversation_member(conversation_member)
+    end
+  end
 end
