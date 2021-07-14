3b26dbbdeb314cf0d34b2f6a4377ccb09d7ab196
ryan <ryanyogan@gmail.com>
2021-07-13 21:04:24 -0500

    added seed file

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,11 +1,14 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     Chatz.Repo.insert!(%Chatz.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+alias Chatz.Auth.User
+alias Chatz.Chat.{Conversation, ConversationMember}
+alias Chatz.{Auth, Chat}
+
+{:ok, %User{id: u1_id}} = Auth.create_user(%{nickname: "User One"})
+{:ok, %User{id: u2_id}} = Auth.create_user(%{nickname: "User Two"})
+
+{:ok, %Conversation{id: conv_id}} = Chat.create_conversation(%{title: "Modern Talking"})
+
+{:ok, %ConversationMember{}} =
+  Chat.create_conversation_member(%{conversation_id: conv_id, user_id: u1_id, owner: true})
+
+{:ok, %ConversationMember{}} =
+  Chat.create_conversation_member(%{conversation_id: conv_id, user_id: u2_id, owner: false})
