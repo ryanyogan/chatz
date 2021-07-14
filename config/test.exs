2b849fd7a370d6dbee8649720bae545bea08a89a
ryan <ryanyogan@gmail.com>
2021-07-13 20:08:49 -0500

    there be some examples for ya

--- config/test.exs
+++ config/test.exs
@@ -1,10 +1,5 @@
 use Mix.Config
 
-# Configure your database
-#
-# The MIX_TEST_PARTITION environment variable can be used
-# to provide built-in test partitioning in CI environment.
-# Run `mix help test` for more information.
 config :chatz, Chatz.Repo,
   username: "postgres",
   password: "postgres",
@@ -12,11 +7,10 @@ config :chatz, Chatz.Repo,
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
 
-# We don't run a server during test. If one is required,
-# you can enable the server option below.
 config :chatz, ChatzWeb.Endpoint,
   http: [port: 4002],
   server: false
 
-# Print only warnings and errors during test
 config :logger, level: :warn
+
+import_config "#{Mix.env()}.secret.exs"
