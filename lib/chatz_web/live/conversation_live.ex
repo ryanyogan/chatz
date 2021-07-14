bb215ce05902d52f9f83cb34470a9e1e8b2a3d11
ryan <ryanyogan@gmail.com>
2021-07-14 09:34:01 -0500

    added pubsub

--- lib/chatz_web/live/conversation_live.ex
+++ lib/chatz_web/live/conversation_live.ex
@@ -1,4 +1,5 @@
 defmodule ChatzWeb.ConversationLive do
+  require Logger
   use ChatzWeb, :live_view
 
   alias Chatz.{Auth, Chat, Repo}
@@ -41,6 +42,8 @@ defmodule ChatzWeb.ConversationLive do
         _url,
         socket
       ) do
+    ChatzWeb.Endpoint.subscribe("conversation_#{conversation_id}")
+
     {:noreply,
      socket
      |> assign(:user_id, user_id)
@@ -74,14 +77,25 @@ defmodule ChatzWeb.ConversationLive do
          }) do
       {:ok, new_message} ->
         new_message = %{new_message | user: user}
-        updated_messages = [new_message | socket.assigns.messages]
 
-        {:noreply, socket |> assign(:messages, updated_messages)}
+        ChatzWeb.Endpoint.broadcast!(
+          "conversation_#{conversation_id}",
+          "new_message",
+          new_message
+        )
 
-      {:error, _} ->
+        {:noreply, socket}
+
+      {:error, err} ->
+        Logger.error(inspect(err))
         {:noreply, socket}
     end
+  end
 
-    {:noreply, socket}
+  @impl true
+  def handle_info(%{event: "new_message", payload: new_message}, socket) do
+    updated_messages = [new_message | socket.assigns.messages]
+
+    {:noreply, socket |> assign(:messages, updated_messages)}
   end
 end
