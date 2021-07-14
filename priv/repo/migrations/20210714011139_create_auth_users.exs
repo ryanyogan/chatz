c738b8b104b5469c0a4ccb821abe7495081bb2a2
ryan <ryanyogan@gmail.com>
2021-07-13 20:11:54 -0500

    added Auth context for users

---
+++ priv/repo/migrations/20210714011139_create_auth_users.exs
@@ -0,0 +1,12 @@
+defmodule Chatz.Repo.Migrations.CreateAuthUsers do
+  use Ecto.Migration
+
+  def change do
+    create table(:auth_users) do
+      add :nickname, :string
+
+      timestamps()
+    end
+
+  end
+end
